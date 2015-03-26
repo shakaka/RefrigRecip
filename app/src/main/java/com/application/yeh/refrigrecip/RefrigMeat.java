bc6949037370e632dd957d16014ec057fb39e1be
shakaka <midknightyeh@hotmail.com>
2015-03-26 09:52:11 +0800

    git with android studio

--- app/src/main/java/com/application/yeh/refrigrecip/RefrigMeat.java
+++ app/src/main/java/com/application/yeh/refrigrecip/RefrigMeat.java
@@ -1,9 +1,11 @@
 package com.application.yeh.refrigrecip;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class RefrigMeat extends ActionBarActivity {
@@ -35,5 +37,22 @@ public class RefrigMeat extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onBeefClick(View view) {
+        Intent intent = new Intent(this, MeatBeef.class);
+        startActivity(intent);
+    }
+
+    public void onPorkClick(View view) {
+    }
+
+    public void onChickenClick(View view) {
+    }
+
+    public void onDuckClick(View view) {
+    }
+
+    public void onLambClick(View view) {
     }
 }
