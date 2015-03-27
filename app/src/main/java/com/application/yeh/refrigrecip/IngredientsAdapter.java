54d52d0e81a79d34379d3060cd07bde309d2e1a4
shakaka <midknightyeh@hotmail.com>
2015-03-27 13:25:03 +0800

    complete ingredientsAdapter

--- app/src/main/java/com/application/yeh/refrigrecip/IngredientsAdapter.java
+++ app/src/main/java/com/application/yeh/refrigrecip/IngredientsAdapter.java
@@ -40,14 +40,20 @@ public class IngredientsAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if(convertView == null)
-            convertView = layoutInflater.inflate(R.layout.search_list_view,parent,false);
+            convertView = layoutInflater.inflate(R.layout.listview_ingredients,parent,false);
 
         ImageView imageView = (ImageView)convertView.findViewById(R.id.image);
-        Bitmap image = BitmapFactory.decodeByteArray(spot.get(position).getImage(), 0, spot.get(position).getImage().length);
+        Bitmap image = BitmapFactory.decodeByteArray(ingred.get(position).getImage(), 0, ingred.get(position).getImage().length);
         imageView.setImageBitmap(image);
 
-        TextView textView=(TextView)convertView.findViewById(R.id.textview);
-        textView.setText(" "+spot.get(position).getId()+" "+spot.get(position).getName()+" (Click me to get more information)");
+        TextView txvName=(TextView)convertView.findViewById(R.id.txvName);
+        txvName.setText(" "+ingred.get(position).getName()+" "+ingred.get(position).getName());
+
+        TextView txvNumber=(TextView)convertView.findViewById(R.id.txvNumber);
+        txvNumber.setText(" "+ingred.get(position).getNumber()+" "+ingred.get(position).getName());
+
+        TextView txvUnit=(TextView)convertView.findViewById(R.id.txvUnit);
+        txvUnit.setText(" "+ingred.get(position).getUnit()+" "+ingred.get(position).getName());
 
 
         return convertView;
